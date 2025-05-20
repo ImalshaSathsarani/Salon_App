888479eb5a05c4c98b66d5b7ecbb5ef52c0702f1
florinsilva <121447369+FlorinNatha@users.noreply.github.com>
2025-05-20 21:37:37 +0530

    updated backend folder structure

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+{/*import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,4 +12,19 @@ export const connectDB = async ()=>{
       console.log(`Error: ${err.message}`);
       process.exit(1);
     }
-}
+}*/}
+const mongoose = require('mongoose');
+
+const ConnectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/salon', {
+      useNewUrlParser: true,
+      userUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch(err) {
+    console.error(err.message);
+    proq.exit(1);
+  }
+};
+module.exports = ConnectDB;
