b7fad02bfd24c086fce021edee600db9883dea1e
Imalsha Sathsarani <158012123+ImalshaSathsarani@users.noreply.github.com>
2025-05-30 23:44:17 +0530

    Created bottom navigation bar

---
+++ frontend/app/(tabs)/bookings.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Bookings() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-primary"
+    >
+      <Text className="text-5xl  text-accent ">Bookings</Text>
+    </View>
+  );
+}
